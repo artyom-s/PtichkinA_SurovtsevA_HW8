5c47d05aeace6402f5247c2b91d73ddd998e3b70
ArtyomSurovtsev <serovtsev.artyom@gmail.com>
2019-08-16 14:17:50 +0300

    Initial Commit

---
+++ PtichkinA_SurovtsevA_HW8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PtichkinA_SurovtsevA_HW8
+//
+//  Created by Артем on 16/08/2019.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
