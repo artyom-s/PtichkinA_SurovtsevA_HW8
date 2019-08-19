21c821cccdfb241374d9e23c9ed75bffa82c6f5f
ArtyomSurovtsev <serovtsev.artyom@gmail.com>
2019-08-19 09:42:53 +0300

    new commit

--- PtichkinA_SurovtsevA_HW8/ModelElements.swift
+++ PtichkinA_SurovtsevA_HW8/ModelElements.swift
@@ -10,7 +10,7 @@ struct ModelElements {
     
     static let shared = ModelElements()
     
-    let headsOrTails = ["🦅", "₽"]
+    let headsOrTails = ["Ⓞ", "Ⓟ"]
     let oneOfThree = ["🧱", "✂️", "📃"]
     let dice = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
     let cards = ["A♤",
